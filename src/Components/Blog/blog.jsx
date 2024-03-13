bc16458c4bcb04cbf56d6a1cbd05e10d3a2a28c7
mahammadshariaralamsarker <154776315+mahammadshariaralamsarker@users.noreply.github.com>
2024-03-13 20:22:35 +0600

    display blog data and hashtags

--- src/Components/Blog/blog.jsx
+++ src/Components/Blog/blog.jsx
@@ -1,14 +1,33 @@
 import PropTypes from 'prop-types'
 
-function Blog({blog}) {
-    console.log(blog);
+function Blog({ blog }) {
+    const { title, cover, reading_time, author,hashtags, author_img, posted_date } = blog;
     return (
-        <div>
+        <div className=''>
+            <img className='w-[600px]' src={cover} alt="" />
+            <div className='flex justify-between'>
+                <div className='flex items-center'>
+                    <img className='w-14 h-14' src={author_img} alt="" />
+                    <div className='  items-center m-5'>
+                        <h3 className='mr-4'>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} Min Read</span>
+                </div>
+            </div>
+            <h1 className="text-2xl">{title}</h1>
+            <p>
+                {
+                    hashtags.map(hash => <span className='mr-5'><a href="">#{hashtags}</a></span>)
+                }
+            </p>
 
         </div>
     )
 }
-Blog.PropTypes={
-    blog:PropTypes.object.isRequired
+Blog.PropTypes = {
+    blog: PropTypes.object.isRequired
 }
 export default Blog
